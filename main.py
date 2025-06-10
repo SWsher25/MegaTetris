8e9b9ad96f39b1db3f39aaf7ccc5e390dce96880
SWsher25 <zakhar.shaidorov@gmail.com>
2025-06-10 20:21:24 +1000

    Commit 1

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from settings import *
 from sys import exit
+from os.path import join
 
 # components
 from game import Game
 from score import Score
 from preview import Preview
+
+from random import choice
 
 class Main():
     def __init__(self):
@@ -14,10 +17,29 @@ class Main():
         self.clock = pygame.time.Clock()
         pygame.display.set_caption("MegaTetris")
 
+        # shapes
+        self.next_shapes = [choice(list(TETROMINOS.keys())) for shape in range(3)]
+        
+
         # components
-        self.game = Game()
+        self.game = Game(self.get_next_shape, self.update_score)
         self.score = Score()
         self.preview = Preview()
+
+        # audio
+        self.music = pygame.mixer.Sound(join("sound", "tetris.mp3"))
+        self.music.set_volume(0.5)
+        self.music.play(-1)
+
+    def update_score(self, lines, score, level):
+        self.score.lines = lines
+        self.score.score = score
+        self.score.level = level
+
+    def get_next_shape(self):
+        next_shape = self.next_shapes.pop(0)
+        self.next_shapes.append(choice(list(TETROMINOS.keys())))
+        return next_shape
 
     def Update(self):
         while True:
@@ -32,7 +54,7 @@ class Main():
             # components
             self.game.Update()
             self.score.Update()
-            self.preview.Update()
+            self.preview.Update(self.next_shapes)
             
             
             
